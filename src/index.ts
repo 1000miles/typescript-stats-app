b40076247fea350da0c23fbc11b1939467e4fb41
Thuy Le <gitnap@mailbox.org>
2020-02-10 02:31:48 +0100

    S11/L101 - Running an Analysis

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,18 @@ const matches = fs
   .map((row: string): string[] => {
     // Split each row at ',' and return
     return row.split(',');
-  })
-console.log(matches);
+  });
+
+// console.log(matches);
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games.`);

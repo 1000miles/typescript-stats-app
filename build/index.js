b18a386afd12fc7ef361e7e346c7eebd10719c08
Thuy Le <gitnap@mailbox.org>
2020-02-10 22:39:24 +0100

    S11/L103 - Using Enums

--- build/index.js
+++ build/index.js
@@ -14,18 +14,42 @@ var matches = fs_1.default
     // Split each row at ',' and return
     return row.split(',');
 });
-console.log(matches);
-var homeWin = 'H';
-var awayWin = 'A';
-var draw = 'D';
+//  console.log(matches);
+// const homeWin = 'H';
+// const awayWin = 'A';
+// const draw = 'D';
+// If props of info in an object chances are less that dataset are being removed when unused
+// const MatchResult = {
+//   HomeWin: 'H',
+//   AwayWin: 'A',
+//   Draw: 'D'
+// }
+// enum - enumerration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+// Way 1:
+// MatchResult.AwayWin
+// MatchResult.HomeWin
+// MatchResult.Draw
+// Way 2 to get MatchResult data
+// const printMatchResult = (): MatchResult => {
+//   if (match[5] === 'H') {
+//     return MatchResult.HomeWin;
+//   }
+//   return MatchResult.AwayWin;
+// }
 var manUnitedWins = 0;
 for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
     var match = matches_1[_i];
     // If name is `Man United` and homewin === 'H
-    if (match[1] === 'Man United' && match[5] === homeWin) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === awayWin) {
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -10,19 +10,49 @@ const matches = fs
     // Split each row at ',' and return
     return row.split(',');
   });
-console.log(matches);
 
-const homeWin = 'H';
-const awayWin = 'A';
-const draw = 'D';
+
+//  console.log(matches);
+
+// const homeWin = 'H';
+// const awayWin = 'A';
+// const draw = 'D';
+
+// If props of info in an object chances are less that dataset are being removed when unused
+// const MatchResult = {
+//   HomeWin: 'H',
+//   AwayWin: 'A',
+//   Draw: 'D'
+// }
+
+// enum - enumerration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
+
+// Way 1:
+// MatchResult.AwayWin
+// MatchResult.HomeWin
+// MatchResult.Draw
+
+// Way 2 to get MatchResult data
+// const printMatchResult = (): MatchResult => {
+//   if (match[5] === 'H') {
+//     return MatchResult.HomeWin;
+//   }
+
+//   return MatchResult.AwayWin;
+// }
 
 let manUnitedWins = 0;
 
 for (let match of matches) {
   // If name is `Man United` and homewin === 'H
-  if (match[1] === 'Man United' && match[5] === homeWin) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === awayWin) {
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }

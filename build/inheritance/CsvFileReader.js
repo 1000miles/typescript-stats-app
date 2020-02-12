06d0049388bcd2eaffc65f472512953338a202c3
Thuy Le <gitnap@mailbox.org>
2020-02-12 23:36:59 +0100

    S11/L116 - Alternate Refactor & Restructure

--- build/inheritance/CsvFileReader.js
+++ build/inheritance/CsvFileReader.js
@@ -4,7 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = __importDefault(require("fs"));
-var utils_1 = require("./utils");
 var CsvFileReader = /** @class */ (function () {
     function CsvFileReader(filename) {
         this.filename = filename;
@@ -14,7 +13,7 @@ var CsvFileReader = /** @class */ (function () {
     }
     ;
     CsvFileReader.prototype.read = function () {
-        console.log(this.data);
+        //console.log(this.data);
         this.data = fs_1.default
             .readFileSync(this.filename, {
             encoding: 'utf-8'
@@ -22,25 +21,12 @@ var CsvFileReader = /** @class */ (function () {
             .split('\n') // Split the string after new line
             // Map through row of strings and return an array of strings
             .map(function (row) {
-            // Split each row at ',' and return
-            return row.split(',');
+            return row.split(','); /* Split each row at ',' and return*/
         })
             // Map through row of strings from football.csv
             // Return an array with Date, string, number or enum MatchResult
-            .map(function (row) {
-            return [
-                // Take the first element of each row and return
-                utils_1.dateStringToDate(row[0]),
-                row[1],
-                row[2],
-                parseInt(row[3]),
-                parseInt(row[4]),
-                // Convert string into matchable enum
-                // MatchResult => Overwrite with Type Assertion
-                row[5],
-                row[6]
-            ];
-        });
+            // !FIXME: TypeError: undefined is not a function
+            .map(this.mapRow); // Just passing function name, no function invokation
     };
     return CsvFileReader;
 }());
--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,31 +1,52 @@
 import fs from "fs";
+import { dateStringToDate } from './utils';
+import { MatchResult } from './MatchResult';
 
-export abstract class CsvFileReader<T> {
+// Tuple
+type MatchData = [
+  Date,
+  string,
+  string,
+  number,
+  number,
+  MatchResult,
+  string
+]
+
+export class CsvFileReader {
   // Initiliaze data as two dimensional data of strings
   // Note: We only use 1x brackets as MatchData[] here since Tuple MatchData is already an array
-  data: T[] = [];
+  data: MatchData[] = [];
 
   constructor(public filename: string) {};
 
-  // Helper
-  abstract mapRow(row: string[]): T;
-
   read(): void {
-    //console.log(this.data);
+    console.log(this.data);
     this.data = fs
       .readFileSync(this.filename, {
         encoding: 'utf-8'
       })
       .split('\n') // Split the string after new line
       // Map through row of strings and return an array of strings
-      .map(
-        (row: string): string[] => {
-          return row.split(','); /* Split each row at ',' and return*/
-        }
-      )
+      .map((row: string): string[] => {
+        // Split each row at ',' and return
+        return row.split(',');
+      })
       // Map through row of strings from football.csv
       // Return an array with Date, string, number or enum MatchResult
-      // !FIXME: TypeError: undefined is not a function
-      .map(this.mapRow) // Just passing function name, no function invokation
+      .map((row: string[]): MatchData => {
+        return [
+          // Take the first element of each row and return
+          dateStringToDate(row[0]), // Example: 2018-08-09T22:00:00.000Z
+          row[1], // Example: 'Man United'
+          row[2], // Example: 'Leicester'
+          parseInt(row[3]), // Example: '2'
+          parseInt(row[4]), // Example: '1'
+          // Convert string into matchable enum
+          // MatchResult => Overwrite with Type Assertion
+          row[5] as MatchResult, // 'H', 'A', 'D'
+          row[6]
+        ]
+      })
   }
 }

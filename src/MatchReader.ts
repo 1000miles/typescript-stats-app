89a718f8080fd3876bef707381a89271591fd7e9
Thuy Le <gitnap@mailbox.org>
2020-02-12 23:50:19 +0100

    S11/L119 - Transforming Data

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -18,22 +18,26 @@ interface DataReader {
 }
 
 export class MatchReader {
+  matches: MatchData[] = [];
+
   constructor(public reader: DataReader) {}
 
-  // Map through row of strings from football.csv
-  // // Return an array with Date, string, number or enum MatchResult
-  // .map((row: string[]): MatchData => {
-  //   return [
-  //     // Take the first element of each row and return
-  //     dateStringToDate(row[0]), // Example: 2018-08-09T22:00:00.000Z
-  //     row[1], // Example: 'Man United'
-  //     row[2], // Example: 'Leicester'
-  //     parseInt(row[3]), // Example: '2'
-  //     parseInt(row[4]), // Example: '1'
-  //     // Convert string into matchable enum
-  //     // MatchResult => Overwrite with Type Assertion
-  //     row[5] as MatchResult, // 'H', 'A', 'D'
-  //     row[6]
-  //   ]
-  // })
+  load(): void {
+    this.reader.read();
+    // map() creates a new array and therefore needs to be assigned to a variable
+    this.matches = this.reader.data.map((row: string[]): MatchData => {
+      return [
+        // Take the first element of each row and return
+        dateStringToDate(row[0]), // Example: 2018-08-09T22:00:00.000Z
+        row[1], // Example: 'Man United'
+        row[2], // Example: 'Leicester'
+        parseInt(row[3]), // Example: '2'
+        parseInt(row[4]), // Example: '1'
+        // Convert string into matchable enum
+        // MatchResult => Overwrite with Type Assertion
+        row[5] as MatchResult, // 'H', 'A', 'D'
+        row[6]
+      ]
+    })
+  }
 }

47f96b7e5c4deb9ed61883deafcf5adbd5ed410a
Thuy Le <gitnap@mailbox.org>
2020-02-13 02:12:54 +0100

    S11/L129 - Generating HTML Reports

--- build/index.js
+++ build/index.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
 var CsvFileReader_1 = require("./CsvFileReader");
-var ConsoleReport_1 = require("./reportTargets/ConsoleReport");
 var WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
 var Summary_1 = require("./Summary");
+var HtmlReport_1 = require("./reportTargets/HtmlReport");
 // Create an object that satisfies the 'DataReader' interface
 var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 // Create an instance of MatchReader add pass in something satifying
@@ -12,5 +12,5 @@ var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 var matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
 //console.log(matchReader);
-var summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
+var summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new HtmlReport_1.HtmlReport());
 summary.buildAndPrintReport(matchReader.matches);
